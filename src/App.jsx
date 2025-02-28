7443897e2f4e185b6c24c8337a4d6d1bcd671dd9
Gaurav Mishra <147796469+Gauravmishra-tempting@users.noreply.github.com>
2025-02-28 21:52:44 +0530

    new code push page, and loader

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react';
+import Loader from './components/loader/Loader';
+
+
+const  MovieApp = lazy(() => import('./components/pages/MovieApp'));
+
+function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< MovieApp />} />
+      </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/components/pages/MovieApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieApp = () => {
+  return (
+    <div>MovieApp</div>
+  )
+}
+
+export default MovieApp
