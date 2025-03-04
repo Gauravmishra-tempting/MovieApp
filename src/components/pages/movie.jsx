e48992e1f770a5ef11cdf8f4075cb0eb7c6b2beb
Gaurav Mishra <147796469+Gauravmishra-tempting@users.noreply.github.com>
2025-03-04 22:17:44 +0530

    add new code

--- src/components/pages/movie.jsx
+++ src/components/pages/movie.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 const Movie = () => {
@@ -12,19 +14,19 @@ const Movie = () => {
     const fetchMovie = async () => {
       try {
         const response = await axios.get(
-          "https://tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com/v1/tmdb?name=robot&page=1",
+          "https://imdb236.p.rapidapi.com/imdb/top250-movies",
           {
             method: 'GET',
             headers: {
               'x-rapidapi-key': api_key,
-              'x-rapidapi-host': 'tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com'
+              'x-rapidapi-host': 'imdb236.p.rapidapi.com'
             },
           }
         );
 
         console.log("API Response:", response.data);
-       if (response.data && response.data.result) {
-        setMovie(response.data.result);
+       if (response.data && response.data) {
+        setMovie(response.data);
         } else {
          setMovie([]);
        }
@@ -53,24 +55,28 @@ const Movie = () => {
 
     <div className="container mt-5">
       <div className="row">
-        <div className="col-md-4">
-          <div class="card">
             {movies.length > 0 ? (
               movies.map((movie) => (
-              <div key={movie.id}>
-              <h1>{movie.title}</h1>
-              <p>{movie.overview}</p>
-              <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde explicabo enim rem odio assumenda?
-              </p>
-              </div>
+        <div className="col" key={movie.id}>
+           <Card className="mb-5 shadow-lg" style={{ width: '18rem', height: '95%', display: 'flex', flexDirection: 'column' }}>
+              <Card.Img variant="top" src={movie.primaryImage}  />
+              <Card.Body>
+                <Card.Subtitle className="mb-2 text-muted">{movie.startYear}</Card.Subtitle>
+                <Card.Title>{movie.primaryTitle}</Card.Title>
+                <Card.Text>
+                   {movie.description}
+                </Card.Text>
+                <Card.Subtitle className='mb-3 text-muted'>Rating : ⭐ {movie.averageRating} / 10</Card.Subtitle>
+                <Card.Subtitle className='mb-3 text-muted'>RunTime : {movie.runtimeMinutes} min</Card.Subtitle>
+                <Button href={movie.url} variant="primary">View on IMDb</Button>
+              </Card.Body>
+            </Card>
+            </div>
               ))
             ) : (
               <h1>No movie found</h1>
             )}
           </div>
-        </div>
-      </div>
     </div>
     </>
   )
